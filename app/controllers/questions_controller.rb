687285add6606ccf19c358a8ef807144c621dd52
Mustafa Farah <mustafafarah8@hotmail.com>
2025-01-06 19:34:30 +0000

    rails stupid coach complete

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,15 @@ class QuestionsController < ApplicationController
     end
 
     def answer
+      @question = params[:question]
+      if @question.blank?
+        @answer = "I can't hear you!"
+      elsif @question =~ /i am going to work/i
+        @answer = "Great!"
+      elsif @question.ends_with?("?")
+        @answer = "Silly question, get dressed and go to work!"
+      else
+        @answer = "I don't care, get dressed and go to work!"
+      end
     end
-
 end
